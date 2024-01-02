6e9b3bc928340e33896008a176cdb724aecc23a1
Luiz Eduardo <luizeduardokrolpereira@gmail.com>
2024-01-01 22:58:31 -0300

    selection sort

--- jsjsjsjs/Algorithms/udemy/Sorting_algorithms/selectionsort.js
+++ jsjsjsjs/Algorithms/udemy/Sorting_algorithms/selectionsort.js
@@ -1,6 +1,19 @@
 function selectsort(arr) {
-
+    for (let i = 0; i < arr.length; i++) {
+        let min = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+        }
+        if (i !== min) {
+            let temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+    }
+    return arr;
 }
 
 
-console.log(selectsort([2,6,1,76,3242,8]))
+console.log(selectsort([2,6,1,76,3242,8,212,45,3,9,8]))
